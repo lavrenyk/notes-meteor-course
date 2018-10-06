e363ad347e9f32a5649f0d4e5821b20e435c62cf
Лавренюк Дмитрий Игоревич <D.I.Lavrenyuk@rtguard.ru>
2018-10-06 22:13:28 +0300

    Mark selected note in list

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import React from "react";
 import { Router, Route, browserHistory } from "react-router";
+import { Session } from 'meteor/session';
 
 import Login from "../ui/Login";
 import Signup from "../ui/Singup";
@@ -25,6 +26,15 @@ const onEnterPrivatePages = () => {
   }
 }
 
+// при заходе на ссылку с id заметки, происходит ее выделение
+const onEnterNotePages = (nextState) => {
+  if (!Meteor.userId()) {
+    browserHistory.replace('/');
+  } else {
+    Session.set('selectedNoteId', nextState.params.id);
+  }
+}
+
 // отслеживаем статус пользователя и при необходимости отправляем его в нужное место
 export const onAuthChange = (isAuthenticated) => {
     const pathname = browserHistory.getCurrentLocation().pathname;
@@ -44,7 +54,7 @@ export const routes = (
     <Route path="/" component={Login} onEnter={onEnterPublicPage} />
     <Route path="signup" component={Signup} onEnter={onEnterPublicPage} />
     <Route path="dashboard" component={Dashboard} onEnter={onEnterPrivatePages} />
-    <Route path="dashboard/:id" component={Dashboard} onEnter={onEnterPrivatePages} />
+    <Route path="dashboard/:id" component={Dashboard} onEnter={onEnterNotePages} />
     <Route path="*" component={NotFound}/>
   </Router>
 );
