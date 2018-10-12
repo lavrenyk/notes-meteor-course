52790bffce78b80e64922ab376be6cc5a6c86c34
Лавренюк Дмитрий Игоревич <D.I.Lavrenyuk@rtguard.ru>
2018-10-12 08:38:08 +0300

    Select newly created notes. Sort notes by updatedAt

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 export const NoteListHeader = (props) => {
   return(
     <div>
       <button onClick={() => {
-        props.meteorCall('notes.insert');
+        props.meteorCall('notes.insert', (err, res) => {
+          if (res) {
+            props.Session.set('selectedNoteId', res);
+          }
+        });
       }}>New note</button>
     </div>
   );
 }
 
+NoteListHeader.propTypes = {
+  meteorCall: PropTypes.func.isRequired,
+  Session: PropTypes.object.isRequired
+}
+
 export default withTracker(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   }
 })(NoteListHeader);
